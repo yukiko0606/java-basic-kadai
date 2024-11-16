94993348c166074aa71b6c33540bc43cb0f3a40f
yukiko0606 <arashi.stf.0606@gmail.com>
2024-11-16 21:27:19 +0900

    課題15章提出

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,14 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		
+		Car_Chapter15 car = new Car_Chapter15();
+		
+		car.gearChange(3);
+		car.run();
+
+	}
+
+}
---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,30 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+	private int gear = 1;
+	private int speed = 10;
+	
+	//コンストラクタ
+	public Car_Chapter15() {
+		
+	}
+	//ギアの値により速度を変える
+	public void gearChange(int afterGear) {
+		this.speed = switch (afterGear) {
+			case 1 -> 10;
+			case 2 -> 20;
+			case 3 -> 30;
+			case 4 -> 40;
+			case 5 -> 50;
+			default -> 10;
+		
+		};
+		System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
+	}
+	
+	//ギアチェンジ後の速度を表示する
+	public void run() {
+		System.out.println("速度は時速" + this.speed + "kmです");
+	}
+	
+}
