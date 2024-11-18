00427001c9b464d0d04a0bfd5840a44cfa1f2384
yukiko0606 <arashi.stf.0606@gmail.com>
2024-11-18 20:50:49 +0900

    Update Kato_Chapter18.java

--- kadai_018/Kato_Chapter18.java
+++ kadai_018/Kato_Chapter18.java
@@ -1,15 +1,15 @@
 package kadai_018;
 
 abstract public class Kato_Chapter18 {
-	public String familyName;
+	public String familyName = "加藤";
 	public String givenName;
-	public String address;
+	public String address = "東京都中野区〇×";
 	
 	//共通の紹介を出力する
 	public void commonIntroduce() {
 		
-		System.out.println("名前は加藤" + givenName +"です");
-		System.out.println("住所は東京都中野区〇×です");
+		System.out.println("名前は" + familyName + givenName +"です");
+		System.out.println("住所は" + address + "です");
 	
 	}
 	
